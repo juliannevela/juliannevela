8d5b1bcbbd473444775e0ed185383dfa6a46a00f
Julianne Vela <75726146+juliannevela@users.noreply.github.com>
2021-07-06 15:38:34 -0700

    updating layout of portfolio site

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import '../styles/global.css';
+import Theme from '../styles/theme';
 
 library.add(fab);
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <Theme>
+            <Component {...pageProps} />
+        </Theme>
+    );
 }
 
 export default MyApp;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,40 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
     render() {
         return (
-            <Html>
+            <Html lang="en-GB">
                 <Head>
                     <link
-                        rel="preconnect"
-                        href="https://fonts.gstatic.com"
-                    ></link>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
                         rel="stylesheet"
-                    ></link>
+                    />
                 </Head>
                 <body>
                     <Main />
@@ -27,5 +44,3 @@ class MyDocument extends Document {
         );
     }
 }
-
-export default MyDocument;
